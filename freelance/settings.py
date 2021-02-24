8c38313aac73f61c63643b3e9d572d758502ae2b
ziyad00 <ziyad.alotaibe@gmail.com>
2021-02-24 17:53:13 +0300

    heroku deployment

--- freelance/settings.py
+++ freelance/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-#STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-#STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = (
- #   os.path.join(PROJECT_ROOT, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
